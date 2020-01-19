3f7f759eb73b1959b7891c30e7bced2c509526d5
panghu <315782792@qq.com>
2020-01-19 20:21:26 +0800

    Perfect the author controller.js

--- ServerSide/controllers/authorController.js
+++ ServerSide/controllers/authorController.js
@@ -7,3 +7,27 @@ exports.author_list = function(req,res) {
 exports.author_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
 };
+
+exports.author_create_get = function(req,res){
+    res.send('Noy implemented:Author create')
+}
+
+exports.author_create_post = function(req,res){
+    res.send('Not implemented:Author create post')
+}
+
+exports.author_delete_get = function(req,res){
+    res.send('Not implemented:Author delete get')
+}
+
+exports.author_delete_post = function(req,res){
+    res.send('NOT IMPLEMENTED:AUTHOR delete post')
+}
+
+exports.author_update_get = function(req,res){
+    res.send('NOT IMPLEMENTED:AUTHOR UPDATE GET')
+}
+
+exports.author_update_post = function(req,res){
+    res.send('NOT IMPLEMNETED:AUTHOR UPDATE POST')
+}
