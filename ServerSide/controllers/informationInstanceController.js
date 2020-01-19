da4b244e381332ccf9abe38d576cde033bb9f356
panghu <315782792@qq.com>
2020-01-19 20:25:35 +0800

    Perfect the information instance controller

--- ServerSide/controllers/informationInstanceController.js
+++ ServerSide/controllers/informationInstanceController.js
@@ -7,3 +7,27 @@ exports.infoInstance_list = function(req,res){
 exports.infoInstance_detail = function(req,res){
     res.send('NOT IMPLEMENTED: informations detail: ' + req.params.id)
 }
+
+exports.infoInstance_create_get = function(req,res){
+    res.send('NOT IMPLEMENTED:information instance create get')
+}
+
+exports.infoInstance_create_post = function(req,res){
+    res.send('NOT IMPLEMENTED:INFOINSTANCE CREATE GET')
+}
+
+exports.infoInstance_delete_get = function(req,res){
+    res.send('NOT IMPLEMENTED:INFOINSTANCE DELETE GET')
+}
+
+exports.infoInstance_delete_post = function(req,res){
+    res.send('NOT IMPLMENTED:INFOINSTANCE DELETE GET')
+}
+
+exports.infoInstance_update_get = function(req,res){
+    res.send('NOT IMPLEMENTED:INFOINSTANCE DELETE GET')
+}
+
+exports.infoInstance_update_post = function(req,res){
+    res.send('NOT IMPLEMENTED:INFOINSTANCE UPDATE POST')
+}
