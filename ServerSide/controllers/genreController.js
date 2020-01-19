1763ef7b4d3abe6e14626d7ef7c14ef4c444c744
panghu <315782792@qq.com>
2020-01-19 20:35:54 +0800

    Perfect the genre controller

--- ServerSide/controllers/genreController.js
+++ ServerSide/controllers/genreController.js
@@ -8,3 +8,26 @@ exports.genre_detail = function(req,res){
     res.send('NOT IMPLEMENTED:Genre detail:' + req.params.id)
 }
 
+exports.genre_create_get = function(req,res){
+    res.send('NOT IMPLEMENTED:GENRE CREATE GET')
+}
+
+exports.genre_create_post = function(req,res){
+    res.send('NOT IMPLEMENTED:GENRE CREATE POST')
+}
+
+exports.genre_delete_get = function(req,res){
+    res.send('NOT IMPLEMENTED:GENRE DELETE GET')
+}
+
+exports.genre_delete_post = function(req,res){
+    res.send('NOT IMPLEMENTED:GENRE DELETE POST')
+}
+
+exports.genre_update_get = function(req,res){
+    res.send('NOT IMPLEMENTED:GENRE UPDATE GET')
+}
+
+exports.genre_update_post = function(req,res){
+    res.send('NOT IMPLEMENTED:GENRE UPDATE POST')
+}
