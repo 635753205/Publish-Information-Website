89828efe59177ce02059007c132a26e91baef696
panghu <315782792@qq.com>
2020-01-19 20:36:44 +0800

    Perfect the information conrtoller

--- ServerSide/controllers/informationController.js
+++ ServerSide/controllers/informationController.js
@@ -9,5 +9,29 @@ exports.information_list = function(req,res){
 }
 
 exports.information_detail = function(req,res){
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id)
+    res.send('NOT IMPLEMENTED: information detail: ' + req.params.id)
+}
+
+exports.information_create_get = function(req,res){
+    res.send('NOT IMPLEMENTED: information create get')
+}
+
+exports.information_create_post = function(req,res){
+    res.send('NOT IMPLEMENTED:INFORMATION CREATE POST')
+}
+
+exports.information_delete_get = function(req,res){
+    res.send('NOT IMPLEMENTED:INFORMATION DELETE GET')
+}
+
+exports.information_delete_post = function(req,res){
+    res.send('NOT IMPLEMENTED:INFORMATION DELETE POST')
+}
+
+exports.information_update_get = function(req,res){
+    res.send('NOT IMPLEMENTED:INFORMATION UPDATE GET')
+}
+
+exports.information_update_post = function(req,res){
+    res.send('NOT IMPLEMENTED:INFORMATION UPDATE POST')
 }
