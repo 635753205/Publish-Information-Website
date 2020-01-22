4bf7c4922381385e2ef4afc76084ec35f41e733a
panghu <315782792@qq.com>
2020-01-22 23:36:14 +0800

    Perfect the populatedb.js

--- ServerSide/populatedb.js
+++ ServerSide/populatedb.js
@@ -57,10 +57,11 @@ function genreCreate(name, cb) {
   })
 }
 
-function infoCreate(title, author, contents, cb) {
+function infoCreate(title, author, contents, genre,cb) {
   infoDetail = {
     title: title,
     author: author,
+    genre:genre,
     contents: contents
   }
 
@@ -100,7 +101,39 @@ function createAuthors(cb) {
   async.series([
     function (callback) {
       authorCreate('yuqing', 'han', callback)
+    },
+    function(callback){
+      authorCreate('悟能', '朱', callback)
+    },
+    function(callback){
+      authorCreate('八戒', '猪', callback)
+    },
+    function(callback){
+      authorCreate('悟能', '朱', callback)
+    },
+    function(callback){
+      authorCreate('元帅', '天蓬', callback)
+    },
+    function(callback){
+      authorCreate('皮龟', '憨', callback)
+    },
+    function(callback){
+      authorCreate('碧落', '乔', callback)
+    },
+    function(callback){
+      authorCreate('亦凡', '吴', callback)
+    },
+    function(callback){
+      authorCreate('徐坤', '菜', callback)
+    },
+    function(callback){
+      authorCreate('瑗', '王', callback)
+    },
+
+    function(callback){
+      authorCreate('寒', '撸', callback)
     }
+
   ], cb)
 }
 
@@ -108,15 +141,57 @@ function createGenres(cb) {
   async.series([
     function (callback) {
       genreCreate('公告', callback)
-    }
+    },
+
+    function(callback){
+      genreCreate('通知', callback)
+    },
+    function(callback){
+      genreCreate('失物招领', callback)
+    },
+    function(callback){
+      genreCreate('寻物启事', callback)
+    },
+
   ], cb)
 }
 
 function createInformation(cb) {
   async.parallel([
     function (callback) {
-      infoCreate('安徽师范大学皖江学院2019-2020学年第二学期时间安排', authors[0], '1.开学时间：2020年2月15日报到注册，2月17日正式上课。2.上课时间：2020年2月17日至2020年6月14日（共17周）。3.节假日：清明节、端午节各放假1天，劳动节放假3天。4.暑假:2020年6月29日开始。', callback)
-    }
+      infoCreate('安徽师范大学皖江学院2019-2020学年第二学期时间安排', authors[0], '1.开学时间：2020年2月15日报到注册，2月17日正式上课。2.上课时间：2020年2月17日至2020年6月14日（共17周）。3.节假日：清明节、端午节各放假1天，劳动节放假3天。4.暑假:2020年6月29日开始。', genres[0], callback)
+    },
+
+    function(callback){
+      infoCreate('安徽师范大学皖江学院爱心协会活动通知', authors[1], '通知：爱心协会将于本周六举办爱心捐赠衣服活动，请各位同学积极参加！', genres[0], callback)
+    },
+    function(callback){
+      infoCreate('失物招领',authors[2],'有哪位同学于今天上午遗失了一张银行卡，请失主联系“189XXXXXXXX”！',  genres[2],callback)
+    },
+    function(callback){
+      infoCreate('失物招领',authors[3],'有哪位同学于昨天中午在食堂遗失了一张校园卡，请失主联系“189XXXXXXXX”！',  genres[2],callback)
+    },
+    function(callback){
+      infoCreate('失物招领',authors[4],'有哪位同学于今天下午在操场丢失一件外套，请失主联系“189XXXXXXXX”！',  genres[2],callback)
+    },
+    function(callback){
+      infoCreate('失物招领',authors[5],'有哪位同学于今天上午在图书馆丢失了一本高数书，请失主联系“189XXXXXXXX”！',  genres[2],callback)
+    },
+    function(callback){
+      infoCreate('寻物启事',authors[6],'本人不慎在图书馆丢失一本高数书，有拾到者请与XX先生联系，必有酬谢！',  genres[3],callback)
+    },
+    function(callback){
+      infoCreate('寻物启事',authors[7],'本人不慎在食堂丢失一张校园卡，有拾到者请与XX先生联系，必有酬谢！', genres[3],callback)
+    },
+    function(callback){
+      infoCreate('寻物启事',authors[8],'本人不慎丢失一张银行卡，有拾到者请与XX先生联系，必有酬谢',genres[3],callback)
+    },
+    function(callback){
+      infoCreate('安徽师范大学皖江学院文学社活动通知',authors[9],'通知：皖江学院文学社将于2020年1月15日下午2点在报告厅举办书法大赛，请各位同学积极参加！', genres[1],callback)
+    },
+    function(callback){
+      infoCreate('安徽师范大学皖江学院新闻社活动通知',authors[10],'通知：皖江学院文学社将于2020年1月20日下午2点在报告厅举办“我是小记者”活动，请各位同学积极参加！', genres[1],callback)
+    },
   ], cb)
 }
 
@@ -124,6 +199,36 @@ function createInfoInstance(cb) {
   async.parallel([
     function (callback) {
       infoInstanceCreate(informations[0], '2020/1/18', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[1], '2020/1/19', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[2], '2020/1/13', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[3], '2020/1/12', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[4], '2020/1/11', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[5], '2020/1/10', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[6], '2020/1/12', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[7], '2020/1/9', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[8], '2020/1/11', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[9], '2020/1/15', callback)
+    },
+    function(callback){
+      infoInstanceCreate(informations[10], '2020/1/19', callback)
     }
   ], cb)
 }
