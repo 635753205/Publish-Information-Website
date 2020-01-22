bbab74af589aaad5f4bdc872462b764163d6a628
panghu <315782792@qq.com>
2020-01-22 23:30:31 +0800

    Perfect the authorController.js

--- ServerSide/controllers/authorController.js
+++ ServerSide/controllers/authorController.js
@@ -1,7 +1,11 @@
 const Author = require('../models/author')
 
-exports.author_list = function(req,res) {
-    res.send('NOT IMPLEMENTED: Author list');
+exports.author_list = function(req,res,next) {
+    Author.find()
+        .exec(function(err,list_authors){
+            if(err){ return next(err)}
+            res.render('author_list',{title:'Author List',authors_list:list_authors})
+        })
 }
 
 exports.author_detail = function(req, res) {
