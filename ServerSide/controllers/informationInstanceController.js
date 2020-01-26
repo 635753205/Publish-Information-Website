977e0947e9521cacbfa594ea7345f32d69208cf8
panghu <315782792@qq.com>
2020-01-26 23:45:48 +0800

    Add the information instance detail page

--- ServerSide/controllers/informationInstanceController.js
+++ ServerSide/controllers/informationInstanceController.js
@@ -1,15 +1,29 @@
+const async = require('async')
 const informationInstance = require('../models/infoInstance')
+
 exports.infoInstance_list = function(req,res,next){
     informationInstance.find()
     .populate('information')
     .exec(function(err,list_information_instance) {
         if(err){return next(err)}
+        console.log(list_information_instance)
         res.render('infoInstance_list',{title:'Information Instance List',information_instance_list:list_information_instance})
     })
 }
 
-exports.infoInstance_detail = function(req,res){
-    res.send('NOT IMPLEMENTED: informations detail: ' + req.params.id)
+exports.infoInstance_detail = function(req,res,next){
+    informationInstance.findById(req.params.id)
+    .populate('information')
+    .exec(function(err,information_instance){
+        if(err){return next(err)}
+        if(information_instance === null){
+            let err = new Error('Can not find the information instance')
+            err.status = 404
+            return next(err)
+        }
+        console.log(information_instance)
+        res.render('information_instance_detail',{title:`Information:${information_instance.title}`,instance_information:information_instance})
+    })
 }
 
 exports.infoInstance_create_get = function(req,res){
