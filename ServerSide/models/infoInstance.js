7b093f4ff83dd2b107ccd798f762dd04bac86146
panghu <315782792@qq.com>
2020-01-26 23:46:43 +0800

    Fixed can not find the url in infoInstance model

--- ServerSide/models/infoInstance.js
+++ ServerSide/models/infoInstance.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
 
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const infoInstanceSchema = new Schema({
+
     information: { type: Schema.Types.ObjectId, ref: 'information', required: true },
     imprint: { type: String, required: true },
-})
 
+})
 infoInstanceSchema.virtual('url')
-    .get(() => {
-        return `/catalog/infoinstance/${this._id}`
+    .get(function () {
+        console.log(this._id)
+        return `/catalog/informationinstance/${this._id}`
     })
+
 
 module.exports = mongoose.model('informationInstance', infoInstanceSchema)
