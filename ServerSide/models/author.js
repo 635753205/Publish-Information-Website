2e222ba6040e2f03bcf159acca80dcba97481059
panghu <315782792@qq.com>
2020-01-26 23:21:13 +0800

    Update the name property in author

--- ServerSide/models/author.js
+++ ServerSide/models/author.js
@@ -10,7 +10,7 @@ authorSchema.virtual('name')
     .get(function(){
         let fullName = ''
         if (this.first_name && this.last_name) {
-            fullName = `${this.first_name} ${this.last_name}`
+            fullName = `${this.last_name}${this.first_name}`
         }
 
         if (!this.first_name || !this.last_name) {
