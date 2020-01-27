9211f6608ce0601af8b5aa762d390e4ce2e1d995
panghu <315782792@qq.com>
2020-01-27 13:57:29 +0800

    Add the genre create template

--- ServerSide/controllers/genreController.js
+++ ServerSide/controllers/genreController.js
@@ -1,4 +1,5 @@
 const async = require('async')
+const validator = require('express-validator')
 
 const information = require('../models/information')
 const Genre = require('../models/genre')
@@ -11,7 +12,7 @@ exports.genre_list = function (req, res, next) {
         })
 }
 
-exports.genre_detail = function (req, res,next) {
+exports.genre_detail = function (req, res, next) {
     async.parallel({
         genre: function (callback) {
             Genre.findById(req.params.id)
@@ -22,25 +23,70 @@ exports.genre_detail = function (req, res,next) {
             information.find({ 'genre': req.params.id })
                 .exec(callback)
         }
-    },function(err,result){
-        if(err) {return next(err)}
-        if(result.genre === null){
+    }, function (err, result) {
+        if (err) { return next(err) }
+        if (result.genre === null) {
             console.log(`can not find the genre `);
             let err = new Error('Genre not find')
             err.status = 404
             return next(err)
         }
-        res.render('genre_detail',{title:'Genre Detail',genre:result.genre,genre_informations: result.genre_informations})
+        res.render('genre_detail', { title: 'Genre Detail', genre: result.genre, genre_informations: result.genre_informations })
     })
 }
 
-exports.genre_create_get = function (req, res) {
-    res.send('NOT IMPLEMENTED:GENRE CREATE GET')
+exports.genre_create_get = function (req, res, next) {
+    res.render('genre_form', { title: 'Create Genre' })
 }
 
-exports.genre_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED:GENRE CREATE POST')
-}
+exports.genre_create_post = [
+    //Validate that the name field is not empty
+    validator.body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+
+    //Sanitize(escape) the name field
+    validator.sanitizeBody('name').escape(),
+
+    //Process request after validation and sanitization
+    (req, res, next) => {
+        const errors = validator.validationResult(req)
+
+        //Create a genre object with escaped and trimmed data
+        let genre = new Genre({
+            name: req.body.name
+        })
+
+        if (!errors.isEmpty()) {
+            //There are errors. Render the form again with sanitized values/error messages
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() })
+            return
+        }
+        else {
+            //Data from form is valid
+            //Check if Genre with same name already exist
+            Genre.findOne({
+                'name': req.body.name
+            }).exec(function (err, found_genre) {
+                if (err) { return next(err) }
+
+                if (found_genre) {
+                    //Genre exist ,redirect to its detail page
+                    res.redirect(found_genre.url)
+                } else {
+                    genre.save(function (err) {
+                        if (err) { return next(err) }
+                        //Genre saved. Redirect to genre detail page
+                        res.redirect(genre.url)
+                    })
+                }
+
+            })
+        }
+    }
+]
+
+
+
+
 
 exports.genre_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED:GENRE DELETE GET')
