9604c041a13763cddfb4f0ed65f813d1d873bdd4
panghu <315782792@qq.com>
2020-01-27 13:56:50 +0800

    Update the catalog router file
    Fixed can not show author create template file

--- ServerSide/routes/catalog.js
+++ ServerSide/routes/catalog.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router  = express.Router()
+const router = express.Router()
 
 //reuqire controller modules
 const info_controller = require('../controllers/informationController')
@@ -14,79 +14,80 @@ const info_instance_controller = require('../controllers/informationInstanceCont
 
 //information routes
 
-router.get('/',info_controller.index)
+router.get('/', info_controller.index)
 
-router.get('/informations',info_controller.information_list)
+router.get('/informations', info_controller.information_list)
 
-router.get('/information/:id',info_controller.information_detail)
+router.get('/information/create', info_controller.information_create_get)
 
-router.get('/information/create',info_controller.information_create_get)
+router.post('/information/create', info_controller.information_create_post)
 
-router.post('/information/create',info_controller.information_create_post)
+router.get('/information/:id/delete', info_controller.information_delete_get)
 
-router.get('/information/:id/delete',info_controller.information_delete_get)
+router.post('/information/:id/delete', info_controller.information_delete_post)
 
-router.post('/information/:id/delete',info_controller.information_delete_post)
+router.get('/information/:id/update', info_controller.information_update_get)
 
-router.get('/information/:id/update',info_controller.information_update_get)
+router.post('/information/:id/update', info_controller.information_update_post)
 
-router.post('/information/:id/update',info_controller.information_update_post)
+router.get('/information/:id', info_controller.information_detail)
 
 
 //auther routes
 
-router.get('/authors',author_controller.author_list)
+router.get('/authors', author_controller.author_list)
 
-router.get('/author/:id',author_controller.author_detail)
+router.get('/author/create', author_controller.author_create_get)
 
-router.get('/author/create',author_controller.author_create_get)
+router.post('/author/create', author_controller.author_create_post)
 
-router.post('/author/create',author_controller.author_create_post)
+router.get('/author/:id/delete', author_controller.author_delete_get)
 
-router.get('/author/:id/delete',author_controller.author_delete_get)
+router.post('/author/:id/delete', author_controller.author_delete_post)
 
-router.post('/author/:id/delete',author_controller.author_delete_post)
+router.get('/author/:id/update', author_controller.author_update_get)
 
-router.get('/author/:id/update',author_controller.author_update_get)
+router.post('/author/:id/update', author_controller.author_update_post)
 
-router.post('/author/:id/update',author_controller.author_update_post)
+router.get('/author/:id', author_controller.author_detail)
 
 
 //genre routes
-router.get('/genres',genre_controller.genre_list)
+router.get('/genres', genre_controller.genre_list)
 
-router.get('/genre/:id',genre_controller.genre_detail)
+router.get('/genre/create', genre_controller.genre_create_get)
 
-router.get('/genre/create',genre_controller.genre_create_get)
+router.post('/genre/create', genre_controller.genre_create_post)
 
-router.post('/genre/create',genre_controller.genre_create_post)
 
-router.get('/genre/:id/delete',genre_controller.genre_delete_get)
 
-router.post('/genre/:id/delete',genre_controller.genre_delete_post)
+router.get('/genre/:id/delete', genre_controller.genre_delete_get)
 
-router.get('/genre/:id/update',genre_controller.genre_update_get)
+router.post('/genre/:id/delete', genre_controller.genre_delete_post)
 
-router.post('/genre/:id/update',genre_controller.genre_update_post)
+router.get('/genre/:id/update', genre_controller.genre_update_get)
+
+router.post('/genre/:id/update', genre_controller.genre_update_post)
+router.get('/genre/:id', genre_controller.genre_detail)
 
 
 //information instance routes
-router.get('/informationinstances',info_instance_controller.infoInstance_list)
+router.get('/informationinstances', info_instance_controller.infoInstance_list)
 
-router.get('/informationinstance/:id',info_instance_controller.infoInstance_detail)
+router.get('/informationinstance/create', info_instance_controller.infoInstance_create_get)
 
-router.get('/informationinstance/create',info_instance_controller.infoInstance_create_get)
-
-router.post('/informationinstance/create',info_instance_controller.infoInstance_create_post)
+router.post('/informationinstance/create', info_instance_controller.infoInstance_create_post)
 
 
-router.get('/informationinstance/:id/delete',info_instance_controller.infoInstance_delete_get)
+router.get('/informationinstance/:id/delete', info_instance_controller.infoInstance_delete_get)
 
-router.post('/informationinstance/:id/delete',info_instance_controller.infoInstance_delete_post)
+router.post('/informationinstance/:id/delete', info_instance_controller.infoInstance_delete_post)
 
-router.get('/informationinstance/:id/update',info_instance_controller.infoInstance_update_get)
+router.get('/informationinstance/:id/update', info_instance_controller.infoInstance_update_get)
 
-router.post('/informationinstance/:id/update',info_instance_controller.infoInstance_update_post)
+router.post('/informationinstance/:id/update', info_instance_controller.infoInstance_update_post)
+
+router.get('/informationinstance/:id', info_instance_controller.infoInstance_detail)
 
 
 module.exports = router
