0afe344ed9fda2b43828ef06ec213b5d462c790e
panghu <315782792@qq.com>
2020-02-07 12:21:12 +0800

    Require the express session packages

--- ServerSide/app.js
+++ ServerSide/app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser  = require('body-parser');
+var session  = require('express-session')
 var logger = require('morgan');
 //reuqire the mongoose package
 var mongoose = require('mongoose')
@@ -29,6 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
